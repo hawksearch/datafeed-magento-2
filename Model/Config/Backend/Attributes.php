30a930eea56cb7932157e5649ae03755a1a79464
Andrew Stayart <andrew.stayart@americaneagle.com>
2018-08-01 21:36:06 -0500

    fixing bug with exporting "locked" attributes, such as "category_ids".

--- Model/Config/Backend/Attributes.php
+++ Model/Config/Backend/Attributes.php
@@ -16,6 +16,8 @@ use Magento\Framework\Model\Context;
 use Magento\Framework\Model\ResourceModel\AbstractResource;
 use Magento\Framework\Registry;
 use Magento\Catalog\Model\ResourceModel\Product\Attribute\CollectionFactory as AttributeCollectionFactory;
+use Magento\Eav\Model\Entity\Attribute\Config;
+
 
 
 class Attributes extends \Magento\Framework\App\Config\Value
@@ -24,13 +26,30 @@ class Attributes extends \Magento\Framework\App\Config\Value
      * @var AttributeCollectionFactory
      */
     private $attributeCollectionFactory;
+    /**
+     * @var Config
+     */
+    private $attributeConfig;
 
+    /**
+     * Attributes constructor.
+     * @param Context $context
+     * @param Registry $registry
+     * @param ScopeConfigInterface $config
+     * @param TypeListInterface $cacheTypeList
+     * @param AttributeCollectionFactory $attributeCollectionFactory
+     * @param Config $attributeConfig
+     * @param AbstractResource|null $resource
+     * @param AbstractDb|null $resourceCollection
+     * @param array $data
+     */
     public function __construct(
         Context $context,
         Registry $registry,
         ScopeConfigInterface $config,
         TypeListInterface $cacheTypeList,
         AttributeCollectionFactory $attributeCollectionFactory,
+        Config $attributeConfig,
         AbstractResource $resource = null,
         AbstractDb $resourceCollection = null,
         array $data = []
@@ -38,6 +57,7 @@ class Attributes extends \Magento\Framework\App\Config\Value
     {
         parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
         $this->attributeCollectionFactory = $attributeCollectionFactory;
+        $this->attributeConfig = $attributeConfig;
     }
 
     public function afterLoad()
@@ -46,7 +66,12 @@ class Attributes extends \Magento\Framework\App\Config\Value
         $pac = $this->attributeCollectionFactory->create();
         $pac->addSearchableAttributeFilter();
         $values = [];
+        /** @var \Magento\Catalog\Model\ResourceModel\Eav\Attribute $item */
         foreach ($pac as $item) {
+            $locked = $this->attributeConfig->getLockedFields($item);
+            if(isset($locked['is_searchable'])) {
+                continue;
+            }
             $values[] = $item->getAttributeCode();
         }
         $this->setValue(implode(',', $values));
--- Model/Config/Source/Attributes.php
+++ Model/Config/Source/Attributes.php
@@ -9,6 +9,7 @@
 namespace HawkSearch\Datafeed\Model\Config\Source;
 
 use Magento\Catalog\Model\ResourceModel\Product\Attribute\CollectionFactory as AttributeCollection;
+use Magento\Eav\Model\Entity\Attribute\Config;
 
 class Attributes implements \Magento\Framework\Option\ArrayInterface
 {
@@ -16,10 +17,20 @@ class Attributes implements \Magento\Framework\Option\ArrayInterface
      * @var AttributeCollection
      */
     private $attributeCollectionFactory;
+    /**
+     * @var Config
+     */
+    private $attributeConfig;
 
-    public function __construct(AttributeCollection $collectionFactory)
+    /**
+     * Attributes constructor.
+     * @param AttributeCollection $collectionFactory
+     * @param Config $attributeConfig
+     */
+    public function __construct(AttributeCollection $collectionFactory, Config $attributeConfig)
     {
         $this->attributeCollectionFactory = $collectionFactory;
+        $this->attributeConfig = $attributeConfig;
     }
 
     /**
@@ -32,6 +43,10 @@ class Attributes implements \Magento\Framework\Option\ArrayInterface
         $pac = $this->attributeCollectionFactory->create();
         $ret = [];
         foreach ($pac->getItems() as $item) {
+            $locked = $this->attributeConfig->getLockedFields($item);
+            if(isset($locked['is_searchable'])) {
+                continue;
+            }
             $ret[] = ['value' => $item->getAttributeCode(), 'label' => $item->getFrontendLabel()];
         }
         return $ret;
